3559f98364937aa804f9560e8e8d3867d9bc6858
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2023-11-05 19:48:16 -0300

    Change E.g to e.g.

--- kanban-app/src/utils/lib.js
+++ kanban-app/src/utils/lib.js
@@ -10,16 +10,16 @@ function checkIfIsInArray(array, name) {
 
 function returnRandomDescription() {
   const descriptions = [
-    "E.g Debug a critical software issue in the production environment.",
-    "E.g Conduct a code review for the new feature branch.",
-    "E.g Set up a CI/CD pipeline for the project.",
-    "E.g Implement OAuth 2.0 authentication for the web application.",
-    "E.g Optimize database queries for better performance.",
-    "E.g Create user stories for the next sprint in JIRA.",
-    "E.g Test the application on multiple browsers and devices.",
-    "E.g Research and implement a new JavaScript framework for the front-end.",
-    "E.g Write unit tests for the API endpoints.",
-    "E.g Deploy the latest updates to the cloud server."
+    "e.g. Debug a critical software issue in the production environment.",
+    "e.g. Conduct a code review for the new feature branch.",
+    "e.g. Set up a CI/CD pipeline for the project.",
+    "e.g. Implement OAuth 2.0 authentication for the web application.",
+    "e.g. Optimize database queries for better performance.",
+    "e.g. Create user stories for the next sprint in JIRA.",
+    "e.g. Test the application on multiple browsers and devices.",
+    "e.g. Research and implement a new JavaScript framework for the front-end.",
+    "e.g. Write unit tests for the API endpoints.",
+    "e.g. Deploy the latest updates to the cloud server."
   ];
   
   return descriptions[Math.floor(Math.random() * descriptions.length)];
@@ -27,26 +27,26 @@ function returnRandomDescription() {
 
 function returnRandomSubtask() {
   const taskTitles = [
-    "E.g Fix a critical bug in the login system",
-    "E.g Review and merge pull requests for feature X",
-    "E.g Configure a load balancer for the web servers",
-    "E.g Upgrade the development environment to Node.js 16",
-    "E.g Migrate the database to a NoSQL solution",
-    "E.g Create a wireframe for the new mobile app",
-    "E.g Perform security scans on the application",
-    "E.g Implement multi-factor authentication",
-    "E.g Optimize website images for faster loading",
-    "E.g Write documentation for the REST API",
-    "E.g Integrate a third-party API for payment processing",
-    "E.g Set up automated testing with Selenium",
-    "E.g Optimize Docker containers for resource efficiency",
-    "E.g Conduct a usability test for the user interface",
-    "E.g Refactor the legacy codebase",
-    "E.g Create a technical presentation for stakeholders",
-    "E.g Set up version control with Git",
-    "E.g Implement responsive design for the website",
-    "E.g Test cross-browser compatibility",
-    "E.g Troubleshoot a network issue in the office"
+    "e.g. Fix a critical bug in the login system",
+    "e.g. Review and merge pull requests for feature X",
+    "e.g. Configure a load balancer for the web servers",
+    "e.g. Upgrade the development environment to Node.js 16",
+    "e.g. Migrate the database to a NoSQL solution",
+    "e.g. Create a wireframe for the new mobile app",
+    "e.g. Perform security scans on the application",
+    "e.g. Implement multi-factor authentication",
+    "e.g. Optimize website images for faster loading",
+    "e.g. Write documentation for the REST API",
+    "e.g. Integrate a third-party API for payment processing",
+    "e.g. Set up automated testing with Selenium",
+    "e.g. Optimize Docker containers for resource efficiency",
+    "e.g. Conduct a usability test for the user interface",
+    "e.g. Refactor the legacy codebase",
+    "e.g. Create a technical presentation for stakeholders",
+    "e.g. Set up version control with Git",
+    "e.g. Implement responsive design for the website",
+    "e.g. Test cross-browser compatibility",
+    "e.g. Troubleshoot a network issue in the office"
   ];
 
   return taskTitles[Math.floor(Math.random() * taskTitles.length)];
